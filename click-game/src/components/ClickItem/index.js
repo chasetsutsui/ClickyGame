66852f85382a26b0364d2ad838bc220360855885
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-02-29 20:22:02 -0800

    finished clickygame

---
+++ click-game/src/components/ClickItem/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ClickItem(props) {
+    return (
+        <div
+            role="img"
+            aria-label="click item"
+            onClick={() => props.handleClick(props.id)}
+        />
+    );
+}
+
+export default ClickItem;
